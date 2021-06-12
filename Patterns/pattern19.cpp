533123d5c4c3bc7bb5136ff7733eb934b6e922ca
Abhinav <17122000abhinav@gmail.com>
2021-06-12 14:35:23 +0530

    commit

---
+++ Patterns/pattern19.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    char k;
+    cout << "Enter number" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        k = 'A'+i;
+        for (int j = 0; j <n; j++)
+        {
+            cout << k << " ";
+            k++;
+        }
+        cout << endl;
+    }
+}
