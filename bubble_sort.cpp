90050a3c225b5bbed0f9b45c6014bf4eb2694723
Abhinav <17122000abhinav@gmail.com>
2021-06-14 18:29:20 +0530

    optimized

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -11,7 +11,7 @@ void mySwap(int &a, int &b)
 
 void bubble_sort(int *arr, int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
