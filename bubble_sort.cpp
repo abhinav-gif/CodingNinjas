286e51a4fe992d88e9210de902e5e1ec05243ac9
Abhinav <17122000abhinav@gmail.com>
2021-06-14 18:18:44 +0530

    commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void mySwap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubble_sort(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                mySwap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int *arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        bubble_sort(arr, n);
+        //print sorted array
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        delete[] arr;
+    }
+}
