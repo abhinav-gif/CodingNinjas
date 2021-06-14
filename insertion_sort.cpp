e8a2a628f70e792e7957be5dc2d4829a4dfa1b92
Abhinav <17122000abhinav@gmail.com>
2021-06-14 21:17:24 +0530

    insertion sort

---
+++ insertion_sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int *arr, int n)
+{
+    int j, key;
+    for (int i = 1; i < n ; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int *arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        insertion_sort(arr, n);
+        //print sorted array
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        delete[] arr;
+    }
+}
